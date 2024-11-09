23e13fd30d6059efbbb28322b33ebb1dcf281b96
Fabian <fa.jaque@duocuc.cl>
2024-11-09 15:31:44 -0300

    Archives upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openGallery() {
+    window.location.href = "gallery.html";
+}
+
+function openIndex() {
+    window.location.href = "index.html";
+}
+
+// Para posicionar los corazones aleatoriamente
+document.querySelectorAll('.heart').forEach(heart => {
+    heart.style.left = `${Math.random() * 100}%`;
+    heart.style.top = `${Math.random() * 100}%`;
+});
