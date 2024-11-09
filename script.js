2d6e30ab7ded9cf41fd34a8e066ce48f33c44f59
Fabian <fa.jaque@duocuc.cl>
2024-11-09 17:05:10 -0300

    gallery improve and upload

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ document.querySelectorAll('.heart').forEach(heart => {
     heart.style.top = `${Math.random() * 100}%`;
 });
 
+function openModal(imgElement) {
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = imgElement.src;
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+
