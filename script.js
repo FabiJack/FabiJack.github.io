7581397afc8cfaa27abba142358f0db8211277a9
Fabian <fa.jaque@duocuc.cl>
2024-11-09 15:52:22 -0300

    .mp3 upload

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ document.querySelectorAll('.heart').forEach(heart => {
     heart.style.left = `${Math.random() * 100}%`;
     heart.style.top = `${Math.random() * 100}%`;
 });
+
+// Verificar si el audio debe estar reproduciéndose
+const audio = document.getElementById('background-audio');
+audio.volume = 0.5; // Ajusta el volumen si es necesario
+
+// Si está definido en localStorage, ajusta la reproducción
+if (localStorage.getItem('isPlaying') === 'true') {
+    audio.play();
+}
+
+// Guardar el estado de reproducción al navegar a la galería
+function openGallery() {
+    localStorage.setItem('isPlaying', !audio.paused);
+    window.location.href = "gallery.html";
+}
+
+// Escuchar el evento para cambiar el estado
+audio.onpause = () => localStorage.setItem('isPlaying', 'false');
+audio.onplay = () => localStorage.setItem('isPlaying', 'true');
